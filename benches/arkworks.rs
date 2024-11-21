446b25745db45bdc8cc7bfbad4c8b3e56062bc6a
mat <matdallen@gmail.com>
2024-11-21 15:44:53 -0800

    add pairing eval and group addition

--- benches/arkworks.rs
+++ benches/arkworks.rs
@@ -1,11 +1,10 @@
 #![allow(unused_imports)]
 #![allow(unused_variables)]
 #![allow(dead_code)]
-// bench docs: https://bheisler.github.io/criterion.rs/book/index.html
 
-use ark_bn254::{Fr as Scalar, G1Projective as G1};
+use ark_bn254::{Fr as Scalar, G1Projective as G1, G2Projective as G2};
 use ark_ff::UniformRand;
-use benches_bn254::{ark_g1_add, ark_g1_scalar_mul};
+use benches_bn254::{ark_g1_add, ark_g1_scalar_mul, ark_pairing, ark_multiple_g1_adds};
 use criterion::{black_box, criterion_group, criterion_main, Criterion};
 use rand::thread_rng;
 
@@ -14,14 +13,25 @@ pub fn bn254_operations(c: &mut Criterion) {
 
     let p1 = G1::rand(&mut rng);
     let p2 = G1::rand(&mut rng);
+    let q1 = G2::rand(&mut rng);
     let s = Scalar::rand(&mut rng);
 
     let mut group = c.benchmark_group("BN254");
 
-    group.bench_function("G1 point addition", |b| b.iter(|| black_box(ark_g1_add(p1, p2))));
+    group.bench_function("G1 point addition", |b| {
+        b.iter(|| black_box(ark_g1_add(p1, p2)))
+    });
 
     group.bench_function("G1 scalar multiplication", |b| {
         b.iter(|| black_box(ark_g1_scalar_mul(p1, s)))
+    });
+
+    group.bench_function("Pairing", |b| {
+        b.iter(|| black_box(ark_pairing(p1, q1)))
+    });
+
+    group.bench_function("1000 G1 additions", |b| {
+        b.iter(|| black_box(ark_multiple_g1_adds(p1, p2, 1000)))
     });
 
     group.finish();
